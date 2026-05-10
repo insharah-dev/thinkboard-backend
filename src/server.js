7aa8adeada62ba15b6f4a38169dc688df9b86ad8
insharahkalam <insharahkalam19@gmail.com>
2026-05-10 11:40:07 +0500

    upd

--- src/server.js
+++ src/server.js
@@ -15,16 +15,10 @@ app.use(express.json())
 
 app.use(
     cors({
-        origin: ["http://localhost:5173", "https://thinkboard-frontend-pink.vercel.app"],
+        origin: ["http://localhost:5173", "https://thinkboard-frontend-eight.vercel.app"],
         credentials: true,
     })
 );
-// app.use(
-//     cors({
-//         origin: ["http://localhost:5173", "https://thinkboard-frontend-eight.vercel.app"],
-//         credentials: true,
-//     })
-// );
 
 
 app.use(rateLimiter)
