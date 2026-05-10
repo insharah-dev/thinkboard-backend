052ff0da5abf89151006fdbea7ccaad96d1be062
insharahkalam <insharahkalam19@gmail.com>
2026-05-10 09:31:52 +0500

    route updated

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -4,9 +4,9 @@ import { createNotes, deleteNotes, getAllNOtes, getNoteById, updateNotes } from 
 const router = express.Router();
 
 router.get("/", getAllNOtes);
-router.get("/:id", getNoteById);
-router.post("/", createNotes);
-router.put("/:id", updateNotes);
-router.delete("/:id", deleteNotes);
+router.get("/getOne/:id", getNoteById);
+router.post("/create", createNotes);
+router.put("/update/:id", updateNotes);
+router.delete("/delete/:id", deleteNotes);
 
 export default router;
